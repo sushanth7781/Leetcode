8f28d90c57406808a8a8e36734f723db8a3df6f3
sushanth7781 <skanuru@gitam.in>
2025-08-26 21:20:11 +0530

    sort list added

---
+++ SortList.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+       
+        if not head or not head.next:
+            return head
+    
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+  
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
+    
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+    
+        curr.next = l1 if l1 else l2
+        return dummy.next
